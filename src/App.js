1e00289a87185c7d4c4b2e0268d4c7344c7ccb2e
Buyaki <rittahbuyaki@gmail.com>
2022-06-01 09:03:40 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-function App() {
+const Footer = () => {
   return (
     <div className="App">
       
---
+++ src/components/handbags/footer.js
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <div class="row">
+      <div class="col-xl-2 col-lg-3 col-md-3 col-3">
+          <button class="btn feedback-btn w-100"> <span class="pl-0 pr-5"> Subscribe </span> <i
+                  class="fas fa-envelope pr-0"></i> </button>
+      </div>
+
+      <div class="col-xl-2 col-lg-3 col-md-3 col-3">
+          <div> +254 722554294 </div>
+          <div> rittahbuyaki@gmail.com </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-3">
+          <p> Mocha Plaza, Opposite Tuskys Echiro </p>
+          <p> Ground Floor, room 1</p>
+          <p>P.O Box 2358-40200 Kisii</p>
+      </div>
+
+      <div class="col-xl-5 col-lg-3 col-md-3 col-3">
+          <div class="fcopyright">
+              <a href="#"> Terms and conditions apply </a> © 1996-2020, handbagscollections.com, Inc.
+          </div>
+          <div class="footer-logos">
+              <i class="fab fa-cc-mastercard"></i>
+              <i class="fab fa-cc-visa"></i>
+              <i class="fab fa-paypal"></i>
+              <i class="fab fa-amazon-pay"></i>
+          </div>
+
+      </div>
+        </div>  
+  );
+}
