f87b35d6cb9027ae41b81ee6a79c22cbdb45d57a
Buyaki01 <rittahbuyaki@gmail.com>
2022-06-06 14:56:28 +0300

    Add HandbagDetails component and install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Handbags from './components/handbags/Handbags';
+import HandbagDetails from './components/handbags/HandbagDetails';
 
 const App = () => (
   <div>
-    <Handbags />
+    <Routes>
+      <Route path="/" element={( <Handbags /> )} />
+      <Route path="/details" element={( <HandbagDetails /> )}/>
+    </Routes>
   </div>
 );
 export default App;
